c7e620d2c8201c772eba5dc9b94371537c9ea721
Bashirabanu <bashirajamadar1@gmail.com>
2023-08-18 16:27:13 +0530

    SECOND COMMIT FOR REACT JS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,21 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Expense Item';
+    const expenseAmount = 999.67;
+    const locationOfExpenditure = 'Restaurant';
+
     return (
-        <div>
+        <div className="expense-item">
+          <div>{expenseDate.toISOString()}</div>
           <h2>Expense Items</h2>
-          <div>
-            <p>Food <span>Rs 10</span></p>
-            <p>Petrol <span>Rs 100</span></p>
-            <p>Movies <span>Rs 200</span></p>
+          <div className="expense-item_s_description">
+          <h2>{expenseTitle}</h2>
+            <p>Food <div className="expense-item__price">Rs{expenseAmount}</div></p>
+            <p>Petrol <div className="expense-item__price">Rs{expenseAmount}</div></p>
+            <p>Movies <div className="expense-item__price">Rs{expenseAmount}</div></p>
+            <p>Location: {locationOfExpenditure}</p> 
           </div>
         </div>
       );
