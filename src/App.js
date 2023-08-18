fb14af2dd567fdd46a22ce2e3030668771090a26
Bashirabanu <bashirajamadar1@gmail.com>
2023-08-18 17:16:53 +0530

    my 3rd commit on react js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      title: 'Food Item', 
+      amount: 500.00, 
+      date: new Date(2021, 2, 9),
+      locationOfExpenditure: 'Restaurant'
+    },
+    {
+      title: 'Petrol', 
+      amount: 999.67, 
+      date: new Date(2021, 2, 18),
+      locationOfExpenditure: 'petrol pump'
+    },
+    {
+      title: 'Movies', 
+      amount: 778.96, 
+      date: new Date(2021, 2, 28),
+      locationOfExpenditure: 'Theater'
+    }
+  ]
   return (
     <div>
       <h2> Lest's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {expenses.map((expense,index) => (
+        <ExpenseItem
+        key={index}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+        locationOfExpenditure={expense.locationOfExpenditure}
+        ></ExpenseItem>
+      ))}
+      {/* <ExpenseItem
+      title={expenses[0].title}
+      amount={expenses[0].amount}
+      date={expenses[0].date}
+      locationOfExpenditure={expenses[0].locationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+      title={expenses[1].title}
+      amount={expenses[1].amount}
+      date={expenses[1].date}
+      locationOfExpenditure={expenses[1].locationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+      title={expenses[2].title}
+      amount={expenses[2].amount}
+      date={expenses[2].date}
+      locationOfExpenditure={expenses[2].locationOfExpenditure}
+      ></ExpenseItem> */}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = 'Expense Item';
-    const expenseAmount = 999.67;
-    const locationOfExpenditure = 'Restaurant';
+function ExpenseItem(props) {
 
     return (
         <div className="expense-item">
-          <div>{expenseDate.toISOString()}</div>
+          <div>{props.date.toISOString()}</div>
           <h2>Expense Items</h2>
-          <div className="expense-item_s_description">
-          <h2>{expenseTitle}</h2>
-            <p>Food <div className="expense-item__price">Rs{expenseAmount}</div></p>
-            <p>Petrol <div className="expense-item__price">Rs{expenseAmount}</div></p>
-            <p>Movies <div className="expense-item__price">Rs{expenseAmount}</div></p>
-            <p>Location: {locationOfExpenditure}</p> 
+          <div className="expense-items__description">
+          <h2>{props.title}</h2>
+            <div className="expense-item__price">Rs{props.amount}</div>
+            <div className="expense-item__location">location:{props.locationOfExpenditure}</div>
           </div>
         </div>
       );
