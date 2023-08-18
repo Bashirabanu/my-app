e330697fbb5b1bb60c1ae9184d291a8ba80a0723
Bashirabanu <bashirajamadar1@gmail.com>
2023-08-18 15:36:43 +0530

    my first commit for react js

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+function ExpenseItem() {
+    return (
+        <div>
+          <h2>Expense Items</h2>
+          <div>
+            <p>Food <span>Rs 10</span></p>
+            <p>Petrol <span>Rs 100</span></p>
+            <p>Movies <span>Rs 200</span></p>
+          </div>
+        </div>
+      );
+
+}
+
+export default ExpenseItem;
