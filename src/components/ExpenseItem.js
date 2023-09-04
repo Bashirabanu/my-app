99d5d55ba51bbae930448b008302f46eb475a1a7
Bashirabanu <bashirajamadar1@gmail.com>
2023-09-05 00:55:51 +0530

    my commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,26 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
+import ExpenseDetails from './ExpenseDetails';
 
 function ExpenseItem(props) {
 
     return (
-        <div className="expense-item">
-          <div>{props.date.toISOString()}</div>
-          <h2>Expense Items</h2>
-          <div className="expense-items__description">
-          <h2>{props.title}</h2>
-            <div className="expense-item__price">Rs{props.amount}</div>
-            <div className="expense-item__location">location:{props.locationOfExpenditure}</div>
-          </div>
-        </div>
-      );
+        // <div className="expense-item">
+        //   <ExpenseDate date={props.date} />
+        //   <h2>Expense Items</h2>
+        //   <div className="expense-items__description">
+        //   <h2>{props.title}</h2>
+        //     <div className="expense-item__price">Rs{props.amount}</div>
+        //     <div className="expense-item__location">location:{props.locationOfExpenditure}</div>
+        //   </div>
+        // </div>
+        <div className='expense-item'>
+        
+        <ExpenseDate date ={props.date} />
+        <ExpenseDetails amount={props.amount}  location={props.locationOfExpenditure}   title={props.title} />
 
+    </div>
+      );
 }
 
 export default ExpenseItem;
